10b396a23f2b71c8b8633a5657294f5e09a6765a
marcopoltergeist <paul_dubor@outlook.fr>
2023-12-12 10:35:03 +0100

    affichage des données

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 
+
+
 const PokemonCard = () => {
+  const pokemon = pokemonList[1];
+
   return (
     <figure>
-      {/* Image du Pokémon */}
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-
-      {/* Nom du Pokémon */}
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 };
+
+
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+  },
+];
+
 
 export default PokemonCard;
