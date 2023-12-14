ca6cf189932137d00b2696171c9847caf6212ff4
marcopoltergeist <paul_dubor@outlook.fr>
2023-12-14 09:08:24 +0100

    button pokedex

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,29 +1,68 @@
-import './App.css'
-
-
-import PokemonCard from './components/PokemonCard';
-
-const App = () => {
-
-  return (
-    <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </div>
-  );
-};
+import { useState } from "react";
 
 const pokemonList = [
   {
-    name: "Bulbasaur",
+    name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "Mew",
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
   },
 ];
 
+const App = () => {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handlePreviousClick = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>{pokemonList[pokemonIndex].name}</h1>
+      {pokemonList[pokemonIndex].imgSrc && (
+        <img
+          src={pokemonList[pokemonIndex].imgSrc}
+          alt={pokemonList[pokemonIndex].name}
+        />
+      )}
+
+      <button onClick={handlePreviousClick} disabled={pokemonIndex === 0}>
+        Précédent
+      </button>
+      <button
+        onClick={handleNextClick}
+        disabled={pokemonIndex === pokemonList.length - 1}
+      >
+        Suivant
+      </button>
+    </div>
+  );
+};
 
 export default App;
-
 
